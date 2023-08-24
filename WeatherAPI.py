a4f73d6320a557ca9899f05f2a708bea7fcb7d7c
Darkyy69 <radiasweetgirl812@gmail.com>
2023-08-24 15:13:42 +0100

    Co-authored-by: RiadBensemmane <RiadBensemmane@users.noreply.github.com>

--- WeatherAPI.py
+++ WeatherAPI.py
@@ -1,6 +1,23 @@
-import os
 import json
-import dotenv
+import requests
 
+location=input("Enter current location:")
+params = {
+  'access_key': 'dc2d2f4409088f1463e06d0cbfb96953',
+  'query': location,
 
-print("Hello Rouji")
+}
+
+api_result = requests.get('http://api.weatherstack.com/current', params)
+
+api_response = api_result.json()
+# print(api_response)
+if 'success' in api_response and api_response['success'] == False:
+    print("\n	API request has failed.")
+else:
+    print("\n")
+    print(u'Current temperature in \033[31m%s\033[0m, \033[1m%s\033[0m is \033[1m%d℃\033[0m' % (api_response['location']['name'],api_response['location']['region'], api_response['current']['temperature']) )
+    print(f"The Weather is \033[1m{api_response['current']['weather_descriptions'][0]}\033[0m")
+    print(f"The Wind speed is \033[1m{api_response['current']['wind_speed']} km/h\033[0m")
+    print(f"The Humidity is \033[1m{api_response['current']['humidity']}%\033[0m")
+    print(f"At  \033[1m{api_response['location']['localtime']}\033[0m")
