71acb80786bad7c0e2214875dfbdf974169f50f9
Darkyy69 <radiasweetgirl812@gmail.com>
2023-08-24 15:41:50 +0100

    Final App...

--- WeatherAPI.py
+++ WeatherAPI.py
@@ -1,24 +1,23 @@
-import json
 import requests
+def getWeather(location) :
+    params = {
+        'access_key': 'dc2d2f4409088f1463e06d0cbfb96953',
+        'query': location,
+        'forecast_days' : 7
+    }
+    api_result = requests.get('http://api.weatherstack.com/current', params)
+    api_response = api_result.json()
+    return api_response
 
 location=input("Enter current location:")
-params = {
-  'access_key': 'dc2d2f4409088f1463e06d0cbfb96953',
-  'query': location,
-  'forecast_days' : 7
-
-}
-
-api_result = requests.get('http://api.weatherstack.com/current', params)
-
-api_response = api_result.json()
-print(api_response)
-if 'success' in api_response and api_response['success'] == False:
+result=getWeather(location)
+# print(result)
+if 'success' in result and result['success'] == False:
     print("\n	API request has failed.")
 else:
     print("\n")
-    print(u'Current temperature in \033[31m%s\033[0m, \033[1m%s\033[0m is \033[1m%d℃\033[0m' % (api_response['location']['name'],api_response['location']['region'], api_response['current']['temperature']) )
-    print(f"The Weather is \033[1m{api_response['current']['weather_descriptions'][0]}\033[0m")
-    print(f"The Wind speed is \033[1m{api_response['current']['wind_speed']} km/h\033[0m")
-    print(f"The Humidity is \033[1m{api_response['current']['humidity']}%\033[0m")
-    print(f"At  \033[1m{api_response['location']['localtime']}\033[0m")
+    print(u'Current temperature in \033[31m%s\033[0m, \033[34m%s\033[0m, \033[32m%s\033[0m is \033[33m%d℃\033[0m' % (result['location']['name'],result['location']['region'], result['location']['country'],result['current']['temperature']) )
+    print(f"The Weather is {result['current']['weather_descriptions'][0]}")
+    print(f"The Wind speed is {result['current']['wind_speed']} km/h")
+    print(f"The Humidity is {result['current']['humidity']}%")
+    print(f"At  {result['location']['localtime']}")
